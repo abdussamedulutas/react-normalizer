de57172ece0f58eac9cfaf4940ac0a9d87192479
Abdussamed <zences-software@hotmail.com>
2021-12-09 00:32:27 +0300

    Style fixings

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React, { Children, useEffect } from "react";
 import useForm from "./utils/useForm";
 import {Provider, store} from"./redux/redux";
 import {render} from "react-dom";
-import "./costum.scss";
+import "./theme/costum.scss";
 import {BrowserRouter,Route,Router,Switch,Link,useHistory} from "react-router-dom";
 import { useDispatch, useSelector, useStore } from "react-redux";
 
